86bc99fc8465dd7d783eaceb367e50a650569be0
segunNew <eeomo007@gmail.com>
2024-10-11 07:28:42 +0100

    add contact us page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,18 +5,41 @@ import heroDesktop from '../../assets/images/hero-desktop.jpg'
 import { Helmet } from 'react-helmet-async';
 import { FaWhatsapp } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
-
+import React, { useRef, useState  } from 'react';
+import emailjs from '@emailjs/browser';
 
 
 
 
 const ContactUs = () => {
+
+  const form = useRef();
+  const [message, setMessage] = useState(null); 
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_cnj393s', 'template_sps0479', form.current, {
+        publicKey: '0zoHpmLCfIabYLn17',
+      })
+      .then(
+        () => {
+          setMessage({ type: 'success', text: 'Message was sent successfully!' });
+        },
+        (error) => {
+          setMessage({ type: 'error', text: 'Failed to send message. Please try again.' });
+        },
+      );
+  };
+
+
     return(
         <main className="second-content">
            <Helmet>
         <title>Contact Us - Dala Air Services</title>
       </Helmet>
-<section className='sections-bottom-pad'>
+<section className=''>
 <div className='icon whatsapp-fixed'>
         <a href="https://wa.me/+2348023185363" target="_blank" rel="noopener noreferrer">
           <FaWhatsapp className='fix-whatsapp-icon' />
@@ -50,8 +73,46 @@ const ContactUs = () => {
       </div>
 
 </section>
+
+
+
+<section className="section">
+  <div className="container contact-us">
+
+    <div>
+    <div className="contact--text">
+    <h2> <span> Your Journey Starts Here: </span> Get in Touch with Us!</h2>
+    <p>
+    At Dala Air Service Ltd, we’re dedicated to making your travel dreams a reality. 
+    Whether you have questions about our packages, need assistance with bookings, 
+    or want personalized travel advice, our team is here to help you every step of the way.
+     Reach out to us, and let’s start planning your next adventure together!
+    </p>
+  </div>
+    </div>
+<div className="form">
+{message && (
+                <div className={`message ${message.type === 'success' ? 'success' : 'error'}`}>
+                  {message.text}
+                </div>
+              )}
+
+<form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+</div>
+  </div>
+
+</section>
         </main>
     )
 }
 
 export default ContactUs
+
